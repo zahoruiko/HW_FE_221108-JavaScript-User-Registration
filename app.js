61b04b23955294568166b17851a8addde8aa229a
zahoruiko <66906889+zahoruiko@users.noreply.github.com>
2023-08-17 12:30:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("JS-script is started.");
 let _html = {};
 
-// Форма для регистрации
+// Registration form
 _html.signUp =
   '<h1>Sign up</h1>' +
   '<div id="message"></div>' +
@@ -20,7 +20,7 @@ _html.signUp =
   '<br /><button id="signInButton" type="button" class="btn btn-outline-primary">Log in</button> ' +
   'or <button id="singUpButton" type="button" class="btn btn-primary">Sign up</button></form>';
 
-// Форма для входа
+// Login form
 _html.signIn =
   '<h1>Log in</h1>' +
   '<span id="message"></span>' +
@@ -35,7 +35,7 @@ _html.signIn =
   '<br /><button id="singUpButton" type="button" class="btn btn-outline-primary">Sign up</button> ' +
   'or <button id="signInButton" type="button" class="btn btn-primary">Log in</button></form>';
 
-// форма для отображения данных пользователя
+// form for displaying user data
 _html.userData =
   '<h1>User account</h1>' +
   '<div id="message"></div>' +
@@ -54,23 +54,23 @@ _html.userData =
   '<br /><button id="signOutButton" type="button" class="btn btn-outline-primary">Log out</button> ' +
   'or <button id="updateButton" type="button" class="btn btn-primary">Update</button></form>';
 
-// Форма для страницы выхода из системы  
+// Form for the logout page  
 _html.signOut = "<h2>You are logged out</h2><hr>";
   '<br /><button id="singUpButton" type="button" class="btn btn-outline-primary">Sign up</button> ' +
   'or <button id="signInButton" type="button" class="btn btn-primary">Log in</button></form>';
 
-// Загружаем начальную форму
+// Loading the initial form
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Start: Sign up form is loaded.");
   showSignUpForm();
 });
 
-// Функция загрузки HTML-фрагментов в контейнер шаблона
+// Loading function of HTML fragments into the template container
 function _appendWrapper(_htmlBlock) {
   document.getElementById("wrapper").innerHTML = _htmlBlock;
 }
 
-// Форма для регистрации и определяем обработку нажатия на кнопки
+// Form for registration and determine the processing of clicking on the buttons
 function showSignUpForm() {
   console.log("Called: showSignUpForm()");
   _appendWrapper(_html.signUp);
@@ -87,7 +87,7 @@ function showSignUpForm() {
     });
 }
 
-// Форма для входа и определяем обработку нажатия на кнопки
+// The entry form and determine the processing of pressing the buttons
 function showSignInForm() {
   console.log("Called: showSignInForm()");
   _appendWrapper(_html.signIn);
@@ -103,7 +103,7 @@ function showSignInForm() {
     }); 
 }
 
-// Форма для отображения данных пользователя и определяем обработку нажатия на кнопки
+// A form for displaying user data and defining the processing of button clicks
 function showUserDataForm() {
   console.log("Called: showUserDataForm()");
   _appendWrapper(_html.userData);
@@ -119,7 +119,7 @@ function showUserDataForm() {
     });
 }
 
-// Форма для выхода из системы и определяем обработку нажатия на кнопки
+// The form for logging out and defining the processing of button clicks
 function showSignOutForm() {
   console.log("Called: showSignOutForm()");
   _appendWrapper(_html.signOut);
@@ -135,19 +135,19 @@ function showSignOutForm() {
     });
 }
 
-// Обработка регистрации пользователя
+// Processing user registration
 async function signUp() {
   console.log("Called: signUp()");
-  // Получаем из формы введенные пользователем данные
+  // We get the data entered by the user from the form
   let username = document.getElementById("username").value.trim();
-  console.log("username = ", username);
+  console.log("Username = ", username);
   let password = document.getElementById("password1").value.trim();
-  console.log("password1 = ", password);
+  console.log("Password1 = ", password);
   let confirmPassword = document.getElementById("password2").value.trim();
-  console.log("password2 = ", confirmPassword);
-  // Создаем массив для хранения частей текста о выявленных проблемах
+  console.log("Password2 = ", confirmPassword);
+  // Creating an array to store parts of the text about the identified problems
   let errorAnswerParts = [];
-  // Проверяем наличие введенных данных и их допустимость
+  // We check the presence of the entered data and their validity
   if (username == "") {
     errorAnswerParts.push("your name");
   }
@@ -160,20 +160,20 @@ async function signUp() {
   if (password !== confirmPassword && password != "" && confirmPassword != "") {
     errorAnswerParts.push("the same password for confirmation");
   }
-  // Если есть ошибки, тогда отображаем их пользователю
-  // и ждем следующего нажатия на кнопку регистрации
+  // If there are errors, then we display them to the user 
+  // and wait for the next click on the registration button
   if (errorAnswerParts.length > 0) {
     document.getElementById("message").innerText =
       "Please enter: " + errorAnswerParts.join(", ").toString();
   } else {
-    // Если ошибки не было, тогда пытаемся зарегистрировать пользователя
-    // Создаем объект данных, которые будет отправлен на удаленный сервер
+    // If there was no error, then we are trying to register the user
+    // Creating a data object that will be sent to a remote server
     let data = {
       username: username,
       password: password,
       confirm_password: confirmPassword,
     };
-    // Инициализируем переменные для выполнения запроса у удаленному серверу
+    // Initialize variables to execute a request from a remote server
     let queryUrl =
       "https://8fd4-2a02-2a57-4052-0-145f-d48e-abbc-890d.eu.ngrok.io/signup";
     let queryBody = {
@@ -186,38 +186,38 @@ async function signUp() {
       },
       body: JSON.stringify(data),
     };
-    // Инициализируем переменную для контроля результата запроса и хранения полученного JSON
+    // Initialize a variable to control the result of the request and store the received JSON
     let processingError = false;
     let responseJson;
     try {
-      // Отправляем запрос и получаем ответ
+      // We send a request and receive a response
       let response = await fetch(queryUrl, queryBody);
-      console.log("response = ", response);
-      // Преобразовыаем полученный ответ в json-формат
+      console.log("Response = ", response);
+      // Converting the received response to json format
       responseJson = await response.json();
-      console.log("responseJson = ", responseJson);
+      console.log("ResponseJson = ", responseJson);
     } catch (error) {
-      // если произошла ошибка сохраняем факт ее свершения в логический флаг
+      // if an error has occurred, we save the fact of its occurrence in a logical flag
       processingError = true;
-      // Отображаем ошибку в консоль и для пользователя
+      // Displaying the error to the console and to the user
       console.error("Error (fetch & json): " + error);
       document.getElementById("message").innerHTML = error.message;
     }
-    // Если ответ на запрос был получен без ошибок
+    // If the response to the request was received without errors
     if (!processingError && responseJson !== undefined) {
       console.log("ResponseJson = " + responseJson);
       console.log("Token = " + responseJson.token);
       localStorage.setItem("token", responseJson.token);  
-      console.log("Загружаем данные для страницы данных пользователя с сервера");
+      console.log("Downloading data for the user's data page from the server");
       getMe();
-    } else { // Если при регистрации произошла ошибка
-      console.error("Оставляем доступной форму регистрации пользователя");
+    } else { // If an error occurred during registration
+      console.error("We leave the user registration form available");
       document.getElementById("message").innerHTML = "An error occurred on the remote server. Try to sign up later. We are working to eliminate the causes of this error";
     }
   }
 }
 
-// Отображение данных пользователя
+// Displaying user data
 async function getMe() {
   console.log("Called: getMe()");
   let token = localStorage.getItem("token");
@@ -234,26 +234,26 @@ async function getMe() {
         "x-access-token": token,
       },
     };
-    // Инициализируем переменную для контроля результата запроса и хранения полученного JSON
+    // Initialize a variable to control the result of the request and store the received JSON
     let processingError = false;
     let responseJson;
     try {
-      // Отправляем запрос и получаем ответ
+      // We send a request and receive a response
       let response = await fetch(queryUrl, queryBody);
-      console.log("response = ", response);
-      // Преобразовыаем полученный ответ в json-формат
+      console.log("Response = ", response);
+      // Converting the received response to json format
       responseJson = await response.json();
-      console.log("responseJson = ", responseJson);
+      console.log("ResponseJson = ", responseJson);
     } catch (error) {
-      // если произошла ошибка сохраняем факт ее свершения в логический флаг
+      // if an error has occurred, we save the fact of its occurrence in a logical flag
       processingError = true;
-      // Отображаем ошибку в консоль и для пользователя
+      // Displaying the error to the console and to the user
       console.error("Error (fetch & json): " + error);
       document.getElementById("message").innerHTML = error.message;
     }
-    // Загружаем HTML-форму для отображения данных пользователя
+    // Uploading an HTML form to display user data
     showUserDataForm();
-    // Если ответ на запрос был получен без ошибок
+    // If the response to the request was received without errors
     if (!processingError && responseJson !== undefined) {
       console.log("ResponseJson = " + responseJson);
       console.log("User id = ", responseJson.id);
@@ -261,47 +261,47 @@ async function getMe() {
       console.log("User age = ", responseJson.age);
       console.log("User avatar = ", responseJson.avatar);
   
-      // Вставляем данные пользователя в HTML-форму
+      // Inserting user data into an HTML form
       document.getElementById("username").value.innerHTML = responseJson.username;
       document.getElementById("age").value.innerHTML = responseJson.age;
       document.getElementById("avatar").value.innerHTML = responseJson.avatar;
     } else {
-      // Если произошла ошибка
-      console.error("Отображаем форму регистрации пользователя и ошибку");
+      // If an error has occurred
+      console.error("Displaying the user registration form and error");
       document.getElementById("message").innerHTML = "An error occurred on the remote server. Try to sign up later. We are working to eliminate the causes of this error";
     }
   } else {
-    // Если токен не определен, тогда отображаем форму для регистрации пользователя
+    // If the token is not defined, then we display a form for user registration
     showSignUpForm();
   }
 }
 
-// Обработка обновления данных пользователя
+// Processing user data updates
 async function updateUserData() {
   console.log("Called: updateUserData()");
   let token = localStorage.getItem("token");
   console.log("Token = ", token);
   if(token != "") {
     let username = document.getElementById("username").value.trim();
-    console.log("username = ", username);
+    console.log("Username = ", username);
     let password = document.getElementById("avatar").value.trim();
-    console.log("password = ", password);
+    console.log("Password = ", password);
     let age = document.getElementById("age").value.trim();
-    console.log("age = ", age);
-    // массив для хранения частей текста о выявленных проблемах
+    console.log("Age = ", age);
+    // array for storing parts of the text about the identified problems
     let errorAnswerParts = []; 
     if (username == "") {
       errorAnswerParts.push("your name (required!)");
     }
-    //  Данные пользователя
+    //  User data
     let userData = {
       username: username,
       avatar: avatar,
       age: age,
     };
 
-    // Если же есть ошибки, тогда отображаем их пользователю
-    // и ждем следующего нажатия на кнопку регистрации
+    // If there are errors, then we display them to the user 
+    // and wait for the next click on the registration button
     if (errorAnswerParts.length > 0) {
       document.getElementById("message").innerText =
         "Please enter: " + errorAnswerParts.join(", ").toString();
@@ -322,38 +322,38 @@ async function updateUserData() {
       let processingError = false;
       let responseJson;
       try {
-        // Отправляем запрос и получаем ответ
+        // We send a request and receive a response
         let response = await fetch(queryUrl, queryBody);
-        console.log("response = ", response);
-        // Преобразовыаем полученный ответ в json-формат
+        console.log("Response = ", response);
+        // Converting the received response to json format
         responseJson = await response.json();
-        console.log("responseJson = ", responseJson);
+        console.log("ResponseJson = ", responseJson);
       } catch (error) {
-        // если произошла ошибка сохраняем факт ее свершения в логический флаг
+        // if an error has occurred, we save the fact of its occurrence in a logical flag
         processingError = true;
-        // Отображаем ошибку в консоль и для пользователя
+        // Displaying the error to the console and to the user
         console.error("Error (fetch & json): " + error);
         document.getElementById("message").innerHTML = error.message;
       }
       
-      // Пытаемся отобразить текущие данные пользователя
+      // Trying to display the current user data
       getMe();
       if (!processingError && responseJson !== undefined) {
         console.log("ResponseJson = " + responseJson);
         document.getElementById("message").innerHTML = "The user data is succefully updated.";
       } else {
-        // Если произошла ошибка
-        console.error("Отображаем ошибку обновления данных пользователя");
+        // If an error has occurred
+        console.error("Displaying the error of updating user data");
         document.getElementById("message").innerHTML = "An error occurred while updating user data. Please try to sign up later. We are working to eliminate the causes of this error.";
       }
     }
   } else {
-    // Если токен не определен, тогда отображаем форму для входа в систему
+    // If the token is not defined, then we display the login form
     showSignInForm();
   }
 }
 
-// Обрабтка выхода из системы
+// Logout Processing
 async function signOut() {
   console.log("Called: signOut()");
   let token = localStorage.getItem("token");
@@ -372,16 +372,16 @@ async function signOut() {
     let processingError = false;
     let responseJson;
     try {
-      // Отправляем запрос и получаем ответ
+      // We send a request and receive a response
       let response = await fetch(queryUrl, queryBody);
       console.log("response = ", response);
-      // Преобразовыаем полученный ответ в json-формат
+      // Converting the received response to json format
       responseJson = await response.json();
       console.log("responseJson = ", responseJson);
     } catch (error) {
-      // если произошла ошибка сохраняем факт ее свершения в логический флаг
+      // if an error has occurred, we save the fact of its occurrence in a logical flag
       processingError = true;
-      // Отображаем ошибку в консоль и для пользователя
+      // Displaying the error to the console and to the user
       console.error("Error (fetch & json): " + error);
       document.getElementById("message").innerHTML = error.message;
     }
@@ -391,20 +391,20 @@ async function signOut() {
       showSignOutForm();
       document.getElementById("message").innerHTML = "You are signed out.";
     } else {
-      // Если произошла ошибка
-      console.error("Ошибка выхода из системы");
-      // Отображаем страницу с данными пользователя
+      // If an error has occurred
+      console.error("Logout error");
+      // Displaying a page with user data
       getMe();
       document.getElementById("message").innerHTML = "You are signed out. Please try later. We are working to eliminate the causes of this error.";
     }
   } else {
-    // Если токен не определен, тогда отображаем форму для регистрации пользователя
+    // If the token is not defined, then we display a form for user registration
     showSignInForm();
     document.getElementById("message").innerHTML = "Your are already signed out.";
   }
 }
 
-// Обработка входа в систему
+// Login processing
 async function signIn() {
   console.log("Called: signIn()");
   let token = localStorage.getItem("token");
@@ -434,37 +434,37 @@ async function signIn() {
     let processingError = false;
     let responseJson;
     try {
-      // Отправляем запрос и получаем ответ
+      // We send a request and receive a response
       let response = await fetch(queryUrl, queryBody);
       console.log("response = ", response);
-      // Преобразовыаем полученный ответ в json-формат
+      // Converting the received response to json format
       responseJson = await response.json();
       console.log("responseJson = ", responseJson);
     } catch (error) {
-      // если произошла ошибка сохраняем факт ее свершения в логический флаг
+      // if an error has occurred, we save the fact of its occurrence in a logical flag
       processingError = true;
-      // Отображаем ошибку в консоль и для пользователя
+      // Displaying the error to the console and to the user
       console.error("Error (fetch & json): " + error);
       document.getElementById("message").innerHTML = error.message;
     }
   
     if (!processingError && responseJson !== undefined) {
       console.log("ResponseJson = " + responseJson);
-      // сохраняем токен сессии
+      // saving the session token
       localStorage.setItem("token", data.token);
-      // Пытаемся отобразить текущие данные пользователя
+      // Trying to display the current user data
       getMe();
       document.getElementById("message").innerHTML = "You are signed in.";
     } else {
-      // Если произошла ошибка
-      console.error("Отображаем ошибку входа пользователя в систему");
-      // Если не удалось войти, тогда отображаем форму для регистрации пользователя
+      // If an error has occurred
+      console.error("Displaying a user login error");
+      // If it was not possible to log in, then we display the user registration form
       showSignInForm();
       document.getElementById("message").innerHTML = "An error occurred while signing in. Please try to sign up later. We are working to eliminate the causes of this error.";
     }
-  } else { // Если пользователь уже вошел (если токен сессии уже сохранен)
-    console.error("Пользователь уже вошел в систему и пытается это сделать еще раз");
-    // Отображаем страницу с данными пользователя
+  } else { // If the user has already logged in (if the session token has already been saved)
+    console.error("The user has already logged in and is trying to do it again");
+    // Displaying a page with user data
     getMe();
     document.getElementById("message").innerHTML = "You are already signed in.";
   }
